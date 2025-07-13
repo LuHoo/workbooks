f2937f93d4504d05d5399b5b188c590efeb7d5c1
LuHoo <lucas.hoogduin@mac.com>
2025-07-13 15:59:14 +0200

    Update install.R
    Adds remotes as package to be able to connect to aicpa

--- .binder/install.R
+++ .binder/install.R
@@ -1,3 +1,3 @@
-install.packages(c("devtools", "readr", "rmarkdown", "ggplot2", "car", "gridExtra", "tidyr", "corrplot", "lmtest", "latex2exp"))
+install.packages(c("remotes", "devtools", "readr", "rmarkdown", "ggplot2", "car", "gridExtra", "tidyr", "corrplot", "lmtest", "latex2exp"))
 devtools::install_github("LuHoo/FSaudit")
-devtools::install_github("LuHoo/aicpa")
+remotes::install_github("LuHoo/aicpa")
