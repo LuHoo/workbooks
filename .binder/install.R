52480ab16aa03729d8a2d2458993b2bd550ceea1
LuHoo <lucas.hoogduin@mac.com>
2025-07-13 15:33:30 +0200

    Update install.R
    Adds car, gridExtra, tidyr, corrplot, lmtest, latex2exp to install.packages list.

--- .binder/install.R
+++ .binder/install.R
@@ -1,3 +1,3 @@
-install.packages(c("devtools", "readr", "rmarkdown", "ggplot2"))
+install.packages(c("devtools", "readr", "rmarkdown", "ggplot2", "car", "gridExtra", "tidyr", "corrplot", "lmtest", latex2exp"))
 devtools::install_github("LuHoo/FSaudit")
 devtools::install_github("LuHoo/aicpa")
